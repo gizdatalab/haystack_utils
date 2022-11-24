dff0c0d7d24f74903d78369a46bfa287bcd2c1e3
prashant <prashant.singh@giz.de>
2022-11-24 17:12:56 +0100

    changing install requires

--- setup.py
+++ setup.py
@@ -6,14 +6,10 @@ install_requires = [
         "spacy==3.2.0",
         "matplotlib==3.5.1",
         "nltk==3.7",
-        "numpy==1.21.6",
-        "pandas==1.2.0",
         "pdfplumber==0.6.2",
         "Pillow==9.1.1",
         "seaborn==0.11.2",
         "transformers==4.21.2",
-        "st-annotated-text==3.0.0",
-        "markdown==3.4.1",
         "summa==1.2.0",
 ]
 
