8fe910cd34315c096fc9b07c4f19f7dbf00b08ae
ppsingh <ppsingh.iitk@gmail.com>
2024-02-18 23:12:40 +0100

    checking auth

--- src/utils/tapp_classifier.py
+++ src/utils/tapp_classifier.py
@@ -40,6 +40,7 @@ def load_targetClassifier(config_file:str = None, classifier_name:str = None):
             classifier_name = config.get('target','MODEL')
     
     logging.info("Loading classifier")  
+    st.write(auth_token)
       
     doc_classifier = pipeline("text-classification", 
                             model=classifier_name, use_auth_token = auth_token,
